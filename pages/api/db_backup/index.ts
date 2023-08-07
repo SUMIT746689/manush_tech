9a97ba32f4d356eeb8020381997ed68cae713cd2
Rakib ul banna <135686564+Rakib-ul-Banna-Elitbuzz@users.noreply.github.com>
2023-08-07 13:00:26 +0600

    db backup added in AllFiles folder

--- pages/api/db_backup/index.ts
+++ pages/api/db_backup/index.ts
@@ -1,22 +1,34 @@
-import { PrismaClient } from '@prisma/client';
 import { exec } from 'child_process'
+import dayjs from 'dayjs';
+import { authenticate } from 'middleware/authenticate';
+import path from "path";
+import fsp from "fs/promises";
 
-const prisma = new PrismaClient();
-
-const index = async (req, res) => {
+const index = async (req, res, refresh_token) => {
     try {
         const { method } = req;
 
         switch (method) {
             case 'POST':
+                if (refresh_token?.role?.title !== 'SUPER_ADMIN') {
+                    throw new Error('Permission denied !')
+                }
+                try {
+                    await fsp.readdir(path.join(process.cwd(), `${process.env.FILESFOLDER}`, "DB_backup"));
+                } catch (error) {
+                    await fsp.mkdir(path.join(process.cwd(), `${process.env.FILESFOLDER}`, "DB_backup"));
+                }
+                const backupPath = path.join(`${process.env.FILESFOLDER}`, "DB_backup", `${dayjs().format('DD-MM-YYYY')}_backup.sql`)
+                
+                //  Backup from docker container
+                // const command = `docker exec mysql /usr/bin/mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} school > ${backupPath}`
+
                 // backup from hard dick
-                // exec(' mysqldump -u root -p[root_password] [database_name] > dumpfilename.sql');
+                const command = `mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} school > ${backupPath}`
 
-                // Backup from docker container
-                exec('docker exec mysql /usr/bin/mysqldump -u root school > backup.sql', (err, stdout, stderr) => {
+                exec(command, (err, stdout, stderr) => {
                     if (err) {
-                        throw new Error('Database backup failed !')
-
+                        return res.status(500).json({ message: 'Database backup failed !' })
                     }
                     res.status(200).json({ message: 'Database backup success !!' });
                 })
@@ -24,13 +36,13 @@ const index = async (req, res) => {
 
                 break;
             default:
-                res.setHeader('Allow', ['GET']);
+                res.setHeader('Allow', ['POST']);
                 res.status(405).end(`Method ${method} Not Allowed`);
         }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: err.message });
+        res.status(500).send(err);
     }
 };
 
-export default index;
+export default authenticate(index);

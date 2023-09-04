8ebd9f74ed2961b508b1caf464b2f706eca57d14
Rakib ul banna <135686564+Rakib-ul-Banna-Elitbuzz@users.noreply.github.com>
2023-09-04 11:49:09 +0600

    db backup

--- pages/api/db_backup/index.ts
+++ pages/api/db_backup/index.ts
@@ -23,10 +23,10 @@ const index = async (req, res, refresh_token) => {
                 const backupPath = path.join(`${process.env.FILESFOLDER}`, "DB_backup", `${dayjs().format('DD-MM-YYYY')}_backup.sql`)
 
                 //  Backup from docker container
-                 const command = `docker exec mysql /usr/bin/mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} school > ${backupPath}`
+                //  const command = `docker exec mysql /usr/bin/mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} school > ${backupPath}`
 
                 // backup from hard dick
-                // const command = `mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} school > ${backupPath}`
+                const command = `mysqldump -u ${process.env.DB_USER} -p ${process.env.DB_PASSWORD} school > ${backupPath}`
 
                 exec(command, async (err, stdout, stderr) => {
                     if (err) {

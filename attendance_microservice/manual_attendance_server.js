70ea281d70e761aa07abab82c7e0db8a16baf5b8
Mehedi Hasan Sumit <72936930+SUMIT746689@users.noreply.github.com>
2023-09-12 10:17:33 +0600

    fix filename

--- attendance_microservice/manual_attendance_server.js
+++ attendance_microservice/manual_attendance_server.js
@@ -7,7 +7,7 @@ const main = async () => {
     let res_tbl_manual_student_attendace_queue = await prisma.tbl_manual_student_attendace_queue.findMany()
     if (res_tbl_manual_student_attendace_queue.length === 0) throw new Error("no manual student attend queue found")
 
-    res_tbl_manual_student_attendace_queue.forEach(async (individual_manual_student_attendace_queue) => {
+    res_tbl_manual_student_attendace_queue.forEach( individual_manual_student_attendace_queue => {
       studentManualQueueHanlde(individual_manual_student_attendace_queue)
     })
   }

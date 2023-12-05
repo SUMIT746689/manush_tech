e34e2ef8806509213efeccee81c93602bbbe0be3
mehedihasansumit <135686469+mehedihasansumit@users.noreply.github.com>
2023-12-05 15:55:51 +0600

    fix attendance

--- attendance_microservice/attendance.js
+++ attendance_microservice/attendance.js
@@ -1,7 +1,5 @@
 import prisma from "./utility/prisma_client.js";
 import { stdAlreadyAttendance, empAlreadyAttendance } from './attendance_utility/findAlreadyAttendance.js'
-import { student_attendence } from "./attendance_utility/createAttendance.js";
-import { isUserAttend } from "./attendance_utility/verifyUserAttend.js";
 // import cron from "node-cron"
 
 const attendance = async () => {
@@ -29,8 +27,7 @@ const attendance = async () => {
                 FROM tbl_attendance_queue as taq
                 JOIN user_role
                 JOIN users ON taq.user_id = users.id
-                WHERE user_role.id = users.role_id 
-                -- AND DATE(taq.submission_time) = DATE(NOW())
+                WHERE user_role.id = users.role_id AND DATE(taq.submission_time) = DATE(NOW())
                 GROUP BY taq.user_id
                 )
 
@@ -38,8 +35,8 @@ const attendance = async () => {
         `
             .then((res) => {
                 // console.log({ res });
-                if (!Array.isArray(res)) return console.log("tbl_attendance_queue response is not array");
-                if (res.length === 0) return console.log("today's tbl_attendance_queue response array length is 0 ");
+                if (!Array.isArray(res)) return console.log("student tbl_attendance_queue response is not array");
+                if (res.length === 0) return console.log("student tbl_attendance_queue response array length is 0 ");
 
                 res.forEach((userAttend) => {
                     const { user_id } = userAttend;
@@ -47,14 +44,11 @@ const attendance = async () => {
                     prisma.student.findFirst({ where: { AND: [{ student_info: { user_id } }, { academic_year: { curr_active: true } }] }, select: { id: true, guardian_phone: true, class_roll_no: true, section: { select: { id: true, name: true, std_entry_time: true, std_exit_time: true, class: { select: { name: true } } } }, student_info: { select: { first_name: true, middle_name: true, last_name: true, school_id: true } } } })
                         .then(async (resStudent) => {
                             const { id, guardian_phone, section, student_info, class_roll_no } = resStudent ?? {};
-                            // console.log({ id, guardian_phone, student_info, section });
-                            if (!id || !section?.std_entry_time) return console.log(`user_id(${user_id}) is not found`);
-                            const isAttend = await stdAlreadyAttendance({ student_id: id, std_min_attend_date_wise, std_max_attend_date_wise })
-                            console.log({ isAttend })
-                            const attendanceStatus = section.std_entry_time.getTime();
-                            console.log({ attendanceStatus, sss: section.std_entry_time })
 
-                            if (isAttend && isAttend?.id) {
+                            if (!id || !section?.std_entry_time) return console.log(`user_id(${user_id}) is not found`);
+                            const isAlreadyAttendanceEntry = await stdAlreadyAttendance({ student_id: id, std_min_attend_date_wise, std_max_attend_date_wise })
+
+                            if (isAlreadyAttendanceEntry && isAlreadyAttendanceEntry?.id) {
                                 prisma.$queryRaw`
                                     SELECT
                                     id,school_id,
@@ -63,24 +57,21 @@ const attendance = async () => {
                                     WHERE user_id = ${user_id} AND submission_time >=${std_min_attend_date_wise} AND submission_time <= ${std_max_attend_date_wise}
                                 `
                                     .then(stdAttendanceQ => {
-                                        console.log({ stdAttendanceQ });
                                         if (stdAttendanceQ.length === 0) return console.log(`student attendence not found for update user_id(${user_id})`);
-                                        const date = new Date(stdAttendanceQ[0].exit_time).getHours();
-                                        console.log({ date })
+
                                         prisma.attendance.update({
-                                            where: { id: isAttend.id },
+                                            where: { id: isAlreadyAttendanceEntry.id },
                                             data: {
                                                 exit_time: stdAttendanceQ[0].exit_time
                                             }
                                         })
-                                            .then(() => console.log(`user_id(${user_id}) update successfull`))
-                                            .catch((err) => `user_id(${user_id}) failed to upload`)
+                                            .then(() => console.log(`user_id(${user_id}) student update successfull`))
+                                            .catch((err) => `user_id(${user_id}) student failed to update`)
                                     })
                                     .catch(err => { console.log({ "error failed student tbl_attendance_queue ": err.message }) })
-
                             }
                             else {
-                                const res = await prisma.$queryRaw`
+                                prisma.$queryRaw`
                                     SELECT
                                         id,
                                         (
@@ -93,41 +84,45 @@ const attendance = async () => {
                                         MAX(submission_time) OVER() AS exit_time
                                     FROM tbl_attendance_queue
                                     WHERE user_id = ${user_id} AND submission_time >=${std_min_attend_date_wise} AND submission_time <= ${std_max_attend_date_wise}
-                                `;
-                                console.log({ res })
-                                let entry_time;
-                                let exit_time;
-                                if (Array.isArray(res) && res.length > 0) {
-                                    entry_time = res[0].entry_time;
-                                    exit_time = res[0].exit_time;
-                                };
-                                prisma.attendance.create({
-                                    data: {
-                                        student_id: id,
-                                        date: new Date(Date.now()),
-                                        status: res[0].status,
-                                        // status: "absent",
-                                        section_id: section.id,
-                                        school_id: student_info.school_id,
-                                        first_name: student_info.first_name,
-                                        middle_name: student_info.middle_name || undefined,
-                                        last_name: student_info.last_name || undefined,
-                                        section_name: section.name,
-                                        class_name: section.class.name,
-                                        class_roll_no,
-                                        entry_time,
-                                        exit_time
-                                    }
-                                })
-                                    .then(res__ => {
-                                        console.log({ "success_request": res__ });
-                                        const deleteIds = res.map((e) => e.id);
-                                        // prisma.tbl_attendance_queue.deleteMany({ where: { id: { in: deleteIds } } })
-                                        //     .then(res => { console.log("tbl_attendance_queue delete successfull") })
-                                        //     .catch(err => { console.log(`error to delete tbl_attendance_queue ids${deleteIds}`, err.message) })
+                                `
+                                    .then(stdAttendanceQ => {
+                                        let entry_time;
+                                        let exit_time;
+                                        if (Array.isArray(stdAttendanceQ) && stdAttendanceQ.length > 0) {
+                                            entry_time = stdAttendanceQ[0].entry_time;
+                                            exit_time = stdAttendanceQ.length > 1 ? stdAttendanceQ[0].exit_time : undefined;
+                                        };
+                                        prisma.attendance.create({
+                                            data: {
+                                                student_id: id,
+                                                date: new Date(Date.now()),
+                                                status: stdAttendanceQ[0].status,
+                                                // status: "absent",
+                                                section_id: section.id,
+                                                school_id: student_info.school_id,
+                                                first_name: student_info.first_name,
+                                                middle_name: student_info.middle_name || undefined,
+                                                last_name: student_info.last_name || undefined,
+                                                section_name: section.name,
+                                                class_name: section.class.name,
+                                                class_roll_no,
+                                                entry_time,
+                                                exit_time
+                                            }
+                                        })
+                                            .then(res__ => {
+                                                console.log({ "success_request": res__ });
+                                                const deleteIds = res.map((e) => e.id);
+                                                // prisma.tbl_attendance_queue.deleteMany({ where: { id: { in: deleteIds } } })
+                                                //     .then(res => { console.log("tbl_attendance_queue delete successfull") })
+                                                //     .catch(err => { console.log(`error to delete tbl_attendance_queue ids${deleteIds}`, err.message) })
+                                            })
+                                            .catch((err) => {
+                                                console.log(`error: create attendaance student_id(${id})`)
+                                            })
                                     })
-                                    .catch((err) => {
-                                        console.log(`error: create attendaance student_id(${id})`)
+                                    .catch(err => {
+                                        console.log(`error get tbl_attendance_queue student_id(${id}) : ${err.message}`)
                                     })
                             }
                         })
@@ -163,13 +158,11 @@ const attendance = async () => {
                 if (resEmp.length === 0) return console.log("today's tbl_attendance_queue response array length is 0 ");
 
                 resEmp.forEach(async (empAttend) => {
-                    console.log({ empAttend })
-                    const isEmpAlreadyAttend = await prisma.employeeAttendance.findFirst({
+                    const isEmpAlreadyEntryAttend = await prisma.employeeAttendance.findFirst({
                         where: { user_id: empAttend.user_id, date: { gte: std_min_attend_date_wise, lte: std_max_attend_date_wise } }
                     })
-                    console.log({ isEmpAlreadyAttend })
-                    if (isEmpAlreadyAttend) {
 
+                    if (isEmpAlreadyEntryAttend) {
                         prisma.$queryRaw`
                             SELECT
                                 id,school_id,
@@ -178,16 +171,16 @@ const attendance = async () => {
                             WHERE user_id = ${empAttend.user_id} AND submission_time >=${std_min_attend_date_wise} AND submission_time <= ${std_max_attend_date_wise}
                         `
                             .then(tblAttendanceQ => {
-                                console.log({ tblAttendanceQ });
-                                if (tblAttendanceQ.length === 0) return console.log(`employee attendence not found for update user_id(${isEmpAlreadyAttend.user_id})`);
+
+                                if (tblAttendanceQ.length === 0) return console.log(`employee attendence not found for update user_id(${isEmpAlreadyEntryAttend.user_id})`);
                                 prisma.employeeAttendance.update({
-                                    where: { id: isEmpAlreadyAttend.id },
+                                    where: { id: isEmpAlreadyEntryAttend.id },
                                     data: {
                                         exit_time: tblAttendanceQ[0].exit_time
                                     }
                                 })
-                                    .then(() => console.log(`user_id(${isEmpAlreadyAttend.user_id}) update successfull`))
-                                    .catch((err) => `user_id(${isEmpAlreadyAttend.user_id}) failed to upload`)
+                                    .then(() => console.log(`employee user_id(${isEmpAlreadyEntryAttend.user_id}) update successfull`))
+                                    .catch((err) => `employee user_id(${isEmpAlreadyEntryAttend.user_id}) failed to update`)
                             })
                             .catch(err => { console.log({ "error failed employee tbl_attendance_queue ": err.message }) })
 
@@ -202,14 +195,12 @@ const attendance = async () => {
                             WHERE user_id = ${empAttend.user_id} AND submission_time >=${std_min_attend_date_wise} AND submission_time <= ${std_max_attend_date_wise}
                         `
                             .then(tblAttendanceQ => {
-                                // console.log({ tblAttendanceQ });
                                 if (tblAttendanceQ.length === 0) return console.log(`tbl_attendance_queue for user_id(${empAttend.user_id})`);
-                                // if (!tblAttendanceQ?.entry_time || !tblAttendanceQ?.exit_time) return;
                                 let entry_time;
                                 let exit_time;
                                 if (Array.isArray(tblAttendanceQ) && tblAttendanceQ.length > 0) {
                                     entry_time = tblAttendanceQ[0].entry_time;
-                                    exit_time = tblAttendanceQ[0].exit_time;
+                                    exit_time = tblAttendanceQ.length > 1 ? tblAttendanceQ[0].exit_time : undefined;
                                 }
                                 prisma.employeeAttendance.create({
                                     data: {
@@ -230,16 +221,15 @@ const attendance = async () => {
                                         //     .catch(err => { console.log(`error to delete tbl_attendance_queue ids${deleteIds}`, err.message) })
 
                                     })
-                                    .catch(err => { console.log({ err }) })
-                                // console.log({ entry_time, exit_time });
+                                    .catch(err => { console.log(`error create employee attendance user_id(${res.user_id})`); })
                             })
-                            .catch(err => { console.log({ err: err.message }) })
+                            .catch(err => { console.log("error employee tbl_qttendance_queue :", err.message) })
 
                     }
                 })
             })
             .catch(err => {
-                console.log({ err })
+                console.log(`error getting all emmployees from tbl_attendance_queue`)
             })
 
         // const resAttendance = await prisma.tbl_attendance_queue.findMany({
@@ -305,8 +295,8 @@ const attendance = async () => {
 
 // setInterval(()=>{
 attendance();
-// },10000)
+// },60000)
 
-// cron.schedule('1 * * * * * *', () => {
-// console.log('running every minute 1, 2, 4 and 5');
+// cron.schedule('* * * * *', () => {
+// console.log('running every minute...');
 //   });

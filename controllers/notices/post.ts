3c987f087ad43ba84a2f97290f3f01a78241b176
mehedihasansumit <135686469+mehedihasansumit@users.noreply.github.com>
2023-12-19 14:13:06 +0600

    notice photo not required

--- controllers/notices/post.ts
+++ controllers/notices/post.ts
@@ -7,7 +7,7 @@ import fsP from 'fs/promises'
 async function post(req, res, refresh_token) {
   try {
     const uploadFolderName = "notice";
-  
+
     const fileType = ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf'];
     const filterFiles = {
       photo: fileType,
@@ -20,22 +20,22 @@ async function post(req, res, refresh_token) {
     const { photo } = files;
     const { title, headLine } = fields;
 
-    if (!title || !headLine || !photo) throw new Error('required all field')
-
-    const photoNewName = Date.now().toString() + '_' + files.photo.originalFilename;
+    if (!title || !headLine) throw new Error('required all field')
 
     let file_url = '';
+    
+    if (photo) {
+      const photoNewName = Date.now().toString() + '_' + files.photo.originalFilename;
+      await fsP.rename(files.photo.filepath, path.join(process.cwd(), `${process.env.FILESFOLDER}`, uploadFolderName, photoNewName))
+        .then(() => {
+          file_url = path.join(uploadFolderName, photoNewName)
 
-    await fsP.rename(files.photo.filepath, path.join(process.cwd(), `${process.env.FILESFOLDER}`, uploadFolderName, photoNewName))
-      .then(() => {
-        file_url = path.join(uploadFolderName, photoNewName)
-
-      })
-      .catch(err => {
-        console.log("err__", err);
-        file_url = path.join(uploadFolderName, files.photo?.newFilename)
-      })
-
+        })
+        .catch(err => {
+          console.log("err__", err);
+          file_url = path.join(uploadFolderName, files.photo?.newFilename)
+        })
+    }
     //fields checks 
 
     const response = await prisma.notice.create({

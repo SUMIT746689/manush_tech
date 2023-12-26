623ccc0eaad9b156cc30e105d5a2f32df1863087
mehedihasansumit <135686469+mehedihasansumit@users.noreply.github.com>
2023-12-26 13:21:35 +0600

    fix services

--- services/sent_sms_service/utility/handleLog.js
+++ services/sent_sms_service/utility/handleLog.js
@@ -1,22 +1,31 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import winston from "winston";
+import 'winston-daily-rotate-file';
 
 const handleLogFile = () => {
-    const { combine, timestamp, printf, colorize, align, json } = winston.format;
+    const { combine, timestamp, printf, align } = winston.format;
+
+    const __filename = fileURLToPath(import.meta.url);
+    const logfilePath = process.env.SCRIPT_ATTENDANCE_LOG_PATH || path.join(__filename, '../../logs');
+
+    const fileRotateTransport = new winston.transports.DailyRotateFile({
+        filename:  `${logfilePath}/sent_sms-%DATE%.log`,
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxFiles: '5d',
+    });
 
     const logger = winston.createLogger({
         level: process.env.LOG_LEVEL || 'info',
         format: combine(
-            // colorize({ all: true }),
             timestamp({
                 format: 'YYYY-MM-DD hh:mm:ss.SSS A',
             }),
-            // align(),
-            json(),
+            align(),
             printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
         ),
-        transports: [new winston.transports.Console()],
+        transports: [fileRotateTransport],
     });
 
     const today = new Date();
@@ -24,10 +33,15 @@ const handleLogFile = () => {
     let mm = today.getMonth() + 1; // month is zero-based
     let dd = today.getDate();
 
-    const __filename = fileURLToPath(import.meta.url);
-    const logfilePath = process.env.SCRIPT_SENT_SMS_LOG_PATH || path.join(__filename, '../../logs');
-    // console.log({logfilePath})
-    logger.add(new winston.transports.File({ filename: `${logfilePath}/sent_sms_${mm}_${dd}_${yyyy}.log` }));
+    // const __filename = fileURLToPath(import.meta.url);
+    // const logfilePath = process.env.SCRIPT_ATTENDANCE_LOG_PATH || path.join(__filename, '../../logs');
+    // logger.add(new winston.transports.File({
+    //     // filename: `${logfilePath}/attendance_${mm}_${dd}_${yyyy}.log`, 
+    //     filename: 'logs/application-%DATE%.log',
+    //     datePattern: `YYYY-MM-DD-HH`,
+    //     zippedArchive: true,
+    //     maxFiles: "5d",
+    // }));
 
     return logger;
 }

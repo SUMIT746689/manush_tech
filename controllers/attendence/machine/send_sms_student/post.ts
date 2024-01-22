976d35953ad61db9c7273aa8a6c8ec76c90c07c8
mehedihasansumit <mehedi@elitbuzz-bd.com>
2024-01-22 18:01:16 +0600

    fix tbl attendance queue

--- controllers/attendence/machine/send_sms_student/post.ts
+++ controllers/attendence/machine/send_sms_student/post.ts
@@ -11,47 +11,6 @@ async function post(req, res, refresh_token) {
       logFile.error("required field is not found")
       return res.status(400).json({ "error": " required field is not found" });
     }
-    // const whereSection = {};
-    // if (section_id) whereSection["id"] = parseInt(section_id);
-
-    // const response = await prisma.class.findFirst({
-    //   where: {
-    //     school_id: refresh_token.school_id,
-    //     id: parseInt(class_id),
-
-    //   },
-    //   select: {
-    //     sections: {
-    //       where: whereSection,
-    //       select: {
-    //         id: true,
-    //         students: {
-    //           select: {
-    //             id: true,
-    //             student_info: {
-    //               select: {
-    //                 user_id: true
-    //               }
-    //             }
-    //           }
-    //         },
-    //       }
-    //     }
-    //   }
-    // });
-
-    const tbl_manual_attendace_queue = [];
-
-    // response?.sections.forEach(section => {
-    //   section.students?.forEach(student => {
-    //     tbl_manual_attendace_queue.push({
-    //       // user_id: student.student_info?.user_id,
-    //       class_id: parseInt(class_id),
-    //       section_id: section_id ? parseInt(section_id) : null,
-    //       // school_id: parseInt(refresh_token?.school_id),
-    //     })
-    //   })
-    // })
 
     await prisma.tbl_student_sent_sms_queue.create({
       data: {

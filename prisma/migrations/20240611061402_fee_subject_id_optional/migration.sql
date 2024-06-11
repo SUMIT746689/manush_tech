08b8357ff46b6fb7ed03f7637c55d2f35918eaad
mehedihasansumit <135686469+mehedihasansumit@users.noreply.github.com>
2024-06-11 16:56:03 +0600

    make subject required

--- prisma/migrations/20240611061402_fee_subject_id_optional/migration.sql
+++ prisma/migrations/20240611061402_fee_subject_id_optional/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `fees` DROP FOREIGN KEY `fees_subject_id_fkey`;
 
 -- AlterTable
-ALTER TABLE `fees` MODIFY `subject_id` INTEGER NULL;
+ALTER TABLE `fees` MODIFY `subject_id` INTEGER NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE `fees` ADD CONSTRAINT `fees_subject_id_fkey` FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;

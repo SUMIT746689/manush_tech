9be42e7fd903927a19dd25a85818102007bb652a
mehedihasansumit <135686469+mehedihasansumit@users.noreply.github.com>
2024-06-11 12:32:54 +0600

    add migration

---
+++ prisma/migrations/20240611061402_fee_subject_id_optional/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE `fees` DROP FOREIGN KEY `fees_subject_id_fkey`;
+
+-- AlterTable
+ALTER TABLE `fees` MODIFY `subject_id` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `fees` ADD CONSTRAINT `fees_subject_id_fkey` FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;

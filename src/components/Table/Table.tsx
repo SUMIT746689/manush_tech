53d4f601a3a6a7c0882e670644fbea591bb64753
mehedihasansumit <135686469+mehedihasansumit@users.noreply.github.com>
2024-06-13 14:00:54 +0600

    variable name change

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,8 @@
-import TableCell from '@mui/material/TableCell';
+import { SxProps } from '@mui/material';
+import TableCell,{TableCellBaseProps} from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
+import { ReactChildren } from 'react';
 
 export const TableRowWrapper = ({ children, ...params }) => {
   return (
@@ -26,25 +28,25 @@ export const TableCellWrapper = ({ children }) => {
   );
 };
 
-export const TableHeaderCellWrapper = ({ children, style = {}, ...parmas }) => {
+export const TableHeaderCellWrapper = ({ children, style = {}, ...params }) => {
   return (
-    <TableCell sx={{ border: '1px solid lightgray', px: 0.6, py: 0, fontSize: { xs: 9, md: 10, xl: 11 }, ...style }} {...parmas}>
+    <TableCell sx={{ border: '1px solid lightgray', px: 0.6, py: 0, fontSize: { xs: 9, md: 10, xl: 11 }, ...style }} {...params}>
       {children}
     </TableCell>
   );
 };
 
-export const TableBodyCellWrapper = ({ children, ...parmas }) => {
+export const TableBodyCellWrapper = ({ children, ...params }) => {
   return (
-    <TableCell sx={{ border: '1px solid lightgray', px: 0.6, py: 0, fontSize: { xs: 10, md: 11, xl: 12 } }} {...parmas}>
+    <TableCell sx={{ border: '1px solid lightgray', px: 0.6, py: 0, fontSize: { xs: 10, md: 11, xl: 12 } }} {...params}>
       {children}
     </TableCell>
   );
 };
 
-export const TableFooterCellWrapper = ({ children, ...parmas }) => {
+export const TableFooterCellWrapper = ({ children, ...params }) => {
   return (
-    <TableCell sx={{ border: '1px solid lightgray', px: 0.6, py: 1, fontSize: { xs: 10, md: 11, xl: 12 },color:"black" }} {...parmas}>
+    <TableCell sx={{ border: '1px solid lightgray', px: 0.6, py: 1, fontSize: { xs: 10, md: 11, xl: 12 },color:"black" }} {...params}>
       {children}
     </TableCell>
   );
